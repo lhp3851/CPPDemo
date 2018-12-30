2df540a49b568a85127c8ce9c25c3d3273d4ea74
Jerry <lhp3851@163.com>
2018-12-30 14:53:01 +0800

    Initial Commit

---
+++ C++Demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Demo
+//
+//  Created by lhp3851 on 2018/12/30.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
